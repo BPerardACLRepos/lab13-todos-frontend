3c6839ff7d3060a07ff95e0e7ee0667262d5394f
Brandon Perard <brandon.perard@gmail>
2021-03-02 18:44:39 -0800

    add userSignUp() to api-utils.js

--- src/api-utils.js
+++ src/api-utils.js
@@ -2,3 +2,10 @@ import request from 'superagent';
 
 const URL = `https://lab-12-auth.herokuapp.com/todos`;
 
+export async function userSignUp(email, password) {
+    const response = await request
+        .post(`${URL}/auth/signup`)
+        .send({ email, password })
+
+    return response.body;  //id, email, token
+}
