cfcd3669b45f1573082c103d15c64d260fb4d1ac
Brandon Perard <brandon.perard@gmail>
2021-03-02 19:06:44 -0800

    add enpoint functions to api-utils.js GET/PUT/POST/DELETE with auth

--- src/api-utils.js
+++ src/api-utils.js
@@ -9,3 +9,52 @@ export async function userSignUp(email, password) {
 
     return response.body;  //id, email, token
 }
+
+export async function userSignIn(email, password) {
+    const response = await request
+        .post(`${URL}/auth/signin`)
+        .send(email, password)
+
+    return response.body;
+}
+
+export async function getAllUserTodos(token) {
+    const response = await request
+        .get(`${URL}/api/todos`)
+        .set('Authorization', token)
+
+    return response.body;
+}
+
+export async function getSingleUserTodo(id, token) {
+    const response = await request
+        .get(`${URL}/api/todos/${id}`)
+        .set('Authorization', token)
+
+    return response.body;
+}
+
+export async function completeUserTodo(id, token) {
+    const response = await request
+        .put(`${URL}/api/todos/${id}`)
+        .set('Authorization', token)
+
+    return response.body;
+}
+
+export async function addUserTodo(todo, token) {
+    const response = await request
+        .post(`${URL}/api/todos`)
+        .set('Authorization', token)
+        .send({ todo })
+
+    return response.body;
+}
+
+export async function deleteUserTodo(id, token) {
+    const response = await request
+        .delete(`${URL}/api/todos/${id}`)
+        .set('Authorization', token)
+
+    return response.body;
+}
